f8efd35a981481abe74243f0f9e585c2d4cfb692
anabadi <anabadi1804@gmail.com>
2017-03-22 14:00:22 -0600

    Add files via upload

---
+++ area.php
@@ -0,0 +1,20 @@
+<!Doctype html>
+<html>
+     <head>
+	      <title>Practica12- Formularios</title>
+		  </head>
+		  <body>
+		       <div>
+			   <form name="formulario" method="post" action="circulo.php">
+                   
+			   radio:<input type="text" name="radio" value="">
+                   
+			   
+			   <input type="submit" value="enviar"/>
+			   </form>
+			  </div>
+			  <a href="index.php">INICIO</a>
+			  <!--Ana cristina,5ª B-->
+			  
+		  </body>
+		  </html>
